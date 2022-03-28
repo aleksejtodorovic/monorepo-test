0bfb2173c0f0bfe336711addef704c44ec9505fb
Aleksej Todorovic <aleksejtodorovic@Aleksejs-Mac-mini.local>
2022-03-28 23:31:15 +0200

    played with basic monorepo

---
+++ basic/packages/components/src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const SomeComponent = () => {
+  return <div>This is some component from shared module</div>;
+};
---
+++ basic/packages/project/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import { SomeComponent } from '@shared/components';
+import axios from 'axios';
+
+function App() {
+    return (
+        <div className='App'>
+            <SomeComponent />
+        </div>
+    );
+}
+
+export default App;
